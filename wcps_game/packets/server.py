db943c392b8bf7c34228f2d759c37dcf7acf2869
SGMartin <s.g.martin94@gmail.com>
2024-07-29 19:08:07 +0200

    Add stub for player authorization packet

--- wcps_game/packets/server.py
+++ wcps_game/packets/server.py
@@ -49,3 +49,29 @@ class LeaveServer(OutPacket):
         )
         self.append(er.SUCCESS)
 
+
+class PlayerAuthorization(OutPacket):
+    class ErrorCodes(Enum):
+        NormalProcedure = 73030     # Please log in using the normal procedure!
+        InvalidPacket = 90100       # Invalid Packet.
+        UnregisteredUser = 90101    # Unregistered User.
+        AtLeast6Chars = 90102       # You must type at least 6 characters .
+        NicknameToShort = 90103     # Nickname should be at least 6 charaters.
+        IdInUseOtherServer = 90104  # Same ID is being used on the server.
+        NotAccessible = 90105       # Server is not accessible.
+        TrainingServer = 90106      # Trainee server is accesible until the rank of a private..
+        ClanWarError = 90112        # You cannot participate in Clan War
+        LackOfResponse = 91010      # Connection terminated because of lack of response for a while.
+        ServerIsFull = 91020        # You cannot connect. Server is full.
+        InfoReqInTrafic = 91030     # Info request are in traffic.
+        AccountUpdateFailed = 91040 # Account update has failed.
+        BadSynchronization = 91050  # User Info synchronization has failed.
+        IdInUse = 92040             # That ID is currently being used.
+        PremiumOnly = 98010         # Available to Premium users only.
+        
+    def __init__(self, error_code:ErrorCodes):
+        super().__init__(
+            packet_id=PacketList.Authorization,
+            xor_key=ClientXorKeys.SEND
+        )
+        self.append(error_code.value)
