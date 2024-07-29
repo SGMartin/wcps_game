a04afa611d2292857e4870073b82627cfc0eaff9
SGMartin <s.g.martin94@gmail.com>
2024-07-29 11:46:40 +0200

    Add servertime packet

---
+++ wcps_game/packets/server.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from enum import Enum
+
+from wcps_core.constants import ErrorCodes as er
+from wcps_core.packets import OutPacket
+from game.game_server import GameServer, User, ClientXorKeys
+
+class ServerTime(OutPacket):
+    class ErrorCodes(Enum):
+        FormatCPrank = 90010           #Format C Drive?
+        DiffrentClientVersion = 90020  #Client version is different. Please download the patch
+        ReInstallWindowsPrank = 90030  #Reinstalling Windows.?
+
+    def __init__(self, this_server:GameServer, error_code:ErrorCodes, u:User=None):
+        super().__init__(
+        packet_id=corepackets.GameServerAuthentication,
+        xor_key=ClientXorKeys.SEND
+        )
+        if error_code != er.SUCCESS or not u:
+            self.append(error_code.value)
+        else:
+            ## get server time
+            self._now = datetime.utcnow()
+            ## 0 offset dates
+            self._month = now.month - 1
+            self._year = now.year - 1900
+            ## get week
+            self._locale.setlocale(locale.LC_TIME, "")
+            self._week = dt.isocalendar()[1]
+            ## build final date block
+            self._date_string = now.strftime("%S/%M/%H/%d") + f"/{self._month}/{self._year}/{self._week}/{self._now.timetuple().tm_yday}/0"
+
+            self.append(er.SUCCESS)
+            self.append(self._date_string)
+
+
+
+
