6e90b907377a9ad4d48dca00dba88ac49ce42c87
SGMartin <s.g.martin94@gmail.com>
2024-08-03 17:31:29 +0200

    Add combined user and stats query

--- wcps_game/database.py
+++ wcps_game/database.py
@@ -72,3 +72,36 @@ async def get_user_stats(username: str) -> dict:
                     return None
             else:
                 return None
+
+
+async def get_user_details_and_stats(username: str) -> dict:
+    async with pool.acquire() as connection:
+        await connection.select_db(settings().database_name)
+        async with connection.cursor() as cur:
+            query = """
+            SELECT 
+                u.id, u.xp, u.money, u.premium, u.premium_expiredate,
+                s.kills, s.deaths, s.headshots, s.bombs_planted, s.bombs_defused, 
+                s.rounds_played, s.flags_taken, s.victories, s.defeats, s.vehicles_destroyed
+            FROM 
+                users u
+            JOIN 
+                user_stats s ON u.id = s.id
+            WHERE 
+                u.username = %s
+            """
+            await cur.execute(query, (username,))
+            user_details = await cur.fetchone()
+            if user_details:
+                this_user = {
+                    "id": int(user_details[0]),
+                    "xp": int(user_details[1]),
+                    "money": int(user_details[2]),
+                    "premium": int(user_details[3]),
+                    "premium_expiredate": int(user_details[4]),
+                    "kills": int(user_details[5]),
+                    "deaths": int(user_details[6])
+                }
+                return this_user
+            else:
+                return None
