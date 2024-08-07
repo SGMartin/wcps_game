9d03209537847538ba87f88671ca57753cb0037d
SGMartin <s.g.martin94@gmail.com>
2024-08-07 17:44:13 +0200

    Add functional chat packet

--- wcps_game/packets/chat.py
+++ wcps_game/packets/chat.py
@@ -23,7 +23,7 @@ class Chat(OutPacket):
 
         super().__init__(
             packet_id=PacketList.CHAT,
-            xor_keys=ClientXorKeys.SEND
+            xor_key=ClientXorKeys.SEND
         )
 
         if error_code != corerr.SUCCESS:
@@ -36,4 +36,18 @@ class Chat(OutPacket):
             self.append(chat_type)
             self.append(receiver_id)
             self.append(receiver_username)
-            self.append(message)  
+            self.append(message_translator(message))
+
+
+def message_translator(human_readable: str):
+    # Define the special characters
+    delimiter = ">>" + chr(0x1D)
+    space_char = chr(0x20)
+    control_char = chr(0x1D)
+
+    # Insert the delimiter at the beginning of the message
+    coded_message = delimiter + human_readable
+
+    # Replace spaces with the control character
+    coded_message = coded_message.replace(space_char, control_char)
+    return coded_message
