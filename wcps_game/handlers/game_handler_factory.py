16c79037d06d032f6f17a99e27480053fbe20be0
SGMartin <s.g.martin94@gmail.com>
2024-08-16 10:25:40 +0200

    Add toggle controllers handlers

--- wcps_game/handlers/game_handler_factory.py
+++ wcps_game/handlers/game_handler_factory.py
@@ -7,6 +7,9 @@ from wcps_game.handlers.game.change_map import ChangeMapHandler
 from wcps_game.handlers.game.change_mode import ChangeGameModeHandler
 from wcps_game.handlers.game.change_rounds_tickets import ChangeRoundsHandler
 from wcps_game.handlers.game.change_kills_tickets import ChangeKillsTicketsHandler
+from wcps_game.handlers.game.change_ping_limit import ChangePingLimitHandler
+from wcps_game.handlers.game.toggle_autostart import ToggleAutoStartHandler
+from wcps_game.handlers.game.toggle_user_limit import ToggleUserLimitHandler
 
 # Dictionary to map packet IDs to handler classes
 HANDLER_MAP = {
@@ -14,7 +17,10 @@ HANDLER_MAP = {
     PacketList.DO_MAP_CLICK: ChangeMapHandler,
     PacketList.DO_TYPE_CLICK: ChangeGameModeHandler,
     PacketList.DO_ROUND_CLICK: ChangeRoundsHandler,
-    PacketList.DO_KILL_CLICK: ChangeKillsTicketsHandler
+    PacketList.DO_KILL_CLICK: ChangeKillsTicketsHandler,
+    PacketList.DO_PING_CLICK: ChangePingLimitHandler,
+    PacketList.DO_AUTOSTART_CLICK: ToggleAutoStartHandler,
+    PacketList.DO_HOLD_CLICK: ToggleUserLimitHandler
 }
 
 
--- wcps_game/packets/packet_list.py
+++ wcps_game/packets/packet_list.py
@@ -39,7 +39,11 @@ class PacketList:
     DO_GAME_PROCESS = 0x7530
 
     # Subpackets of DO_GAME_PROCESS
+    # Lobby related #
     DO_MAP_CLICK = 0x33
     DO_TYPE_CLICK = 0x34
     DO_ROUND_CLICK = 0x35
     DO_KILL_CLICK = 0x37
+    DO_PING_CLICK = 0x3B
+    DO_HOLD_CLICK = 0x3A
+    DO_AUTOSTART_CLICK = 0x3E
